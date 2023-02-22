d4f6c8e9febfa89597e32209285dda23b574c96e
deepAnji281 <84462559+Deepaksaheb@users.noreply.github.com>
2023-02-22 10:19:54 +0530

    signing up the user is done

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,51 @@
+const User=require('../modals/user');
 module.exports.profile=function(req,res)
 {
     return res.render('profile',{title:'user-profile'});
 }
+// rendering the signUp page
+module.exports.signUp=function(req,res)
+{
+    return res.render('UserSignUp',{title:'SignUp'});
+}
+
+// rendering the signIn page
+module.exports.signIn=function(req,res)
+{
+    return res.render('userSignIn',{title:'signIn'});
+}
+
+// crating sign Up
+module.exports.create=function(req,res){
+    if(req.body.password!=req.body.confirm_password)
+    return res.redirect('back');
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err)
+        {
+            console.log('getting error in siginig the data');
+            return;
+        }
+        if(!user)
+        {
+            User.create(req.body,(err,user)=>{
+                if(err)
+                {
+                    return;
+                }
+                return res.redirect('/user/sign-in');
+                
+            })
+
+        }
+        else
+        return res.redirect('back');
+    })
+}
+
+// creating sign In
+
+module.exports.createSession=function(req,res)
+{
+   // to do latter
+}
+
--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const port=7000
 const expressLayouts=require('express-ejs-layouts');
 //linking of mongodb;
 const db=require('./configure/mongoose');
+// require cookies we need to cookie-parser
+const cookieParser=require('cookie-parser');
 // use of layouts
 app.use(expressLayouts);
-
 // use of static file
 app.use(express.static('./assets'));
+app.use(express.urlencoded());
+// use of cookieParser
+app.use(cookieParser());
+// use of static file
 
 // extracting style and scripts
   app.set('layout extractStyles',true);
