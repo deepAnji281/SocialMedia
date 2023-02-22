6c18389e8fca664776c73f4e45969c91cfdf49b4
deepAnji281 <84462559+Deepaksaheb@users.noreply.github.com>
2023-02-22 10:50:35 +0530

    signing the user profile manually done

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,84 @@
+const User=require('../modals/user');
 module.exports.profile=function(req,res)
 {
-    return res.render('profile',{title:'user-profile'});
+    if(req.cookies.user_id)
+    {
+           User.findById(req.cookies.user_id,(err,user)=>{
+            if(err)
+            return;
+            if(user)
+            {
+                return res.render('profile',{title:'user-profile',user:user});
+            }
+            else
+            return res.redirect('/user/sign-in');
+           })
+    }
+    else{
+        return res.redirect('/user/sign-in');
+    }
 }
+// rendering the signUp page
+module.exports.signUp=function(req,res)
+{
+    return res.render('UserSignUp',{title:'SignUp'});
+}
+
+// rendering the signIn page
+module.exports.signIn=function(req,res)
+{
+    return res.render('userSignIn',{title:'signIn'});
+}
+
+// crating sign Up
+module.exports.create=function(req,res){
+    if(req.body.password!=req.body.confirm_password)
+    return res.redirect('back');
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err)
+        {
+            console.log('getting error in siginig the data');
+            return;
+        }
+        if(!user)
+        {
+            User.create(req.body,(err,user)=>{
+                if(err)
+                {
+                    return;
+                }
+                return res.redirect('/user/sign-in');
+                
+            })
+
+        }
+        else
+        return res.redirect('back');
+    })
+}
+
+// creating sign In
+
+module.exports.createSession=function(req,res)
+{
+   // to do latter
+   // find the user
+
+   // user found
+   User.findOne({email:req.body.email},(err,user)=>{
+    if(err)
+    return;
+    if(user)
+    {
+        if(user.password!=req.body.password)
+        return res.redirect('back');
+        // handle create session
+        res.cookie('user_id',user.id);
+        return res.redirect('/user/profile');
+    }
+    else{
+        return res.redirect('back');
+    }
+   })
+}
+
--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,8 @@ const express=require('express');
 const router=express.Router();
 const usercont=require('../controller/userController');
 router.get('/user/profile',usercont.profile);
+router.get('/user/sign-up',usercont.signUp);
+router.get('/user/sign-in',usercont.signIn);
+router.post('/user/create',usercont.create);
+router.post('/user/creatSession',usercont.createSession);
 module.exports=router;
